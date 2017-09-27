3bc177d99a1c4e549187638da6a8a494584993df
JJ Merelo <jjmerelo@gmail.com>
2017-09-27 18:33:43 +0200

    Tests 3 o 5 o 15 listo

--- tests.py
+++ tests.py
@@ -11,6 +11,15 @@ def sumaPositivos( a, b):
     if ( a >= 0 and b >= 0): 
         return a + b
 
+def multiplo3o5o15( numero ):
+    if numero // 15 is int:
+        return 3
+    if numero // 3 is int:
+        return 1
+    if numero // 5 is int:
+        return 2
+    return 0
+    
 class SoloTest(unittest.TestCase):
     
     def testTrue(self):
@@ -19,6 +28,13 @@ class SoloTest(unittest.TestCase):
     def testSuma(self):
         self.assertEqual(sumaPositivos("cadena",3),-1, "Suma correcta")
         self.assertEqual(12,sumaPositivos(4,8), "Suma correcta")
+
+    def testMultiplos(self):
+        self.assertEqual(multiplo3o5o15(3),1,"Multiplo de 3")
+        self.assertEqual(multiplo3o5o15(5),2,u"Multiplo de 5")
+        self.assertEqual(multiplo3o5o15(15),3,u"Multiplo de 15")
+        self.assertEqual(multiplo3o5o15(7),0,u"No es multiplo")
+        
 
 if __name__ == '__main__':
     unittest.main()
