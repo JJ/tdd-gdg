40826b904abf3aea25800b0638208f468d2975a8
guillesiesta <guille.siesta.muriel@gmail.com>
2017-10-09 15:35:20 +0200

    Añadidas funciones resta y multiplicación y sus correspondientes tests

--- tests.py
+++ tests.py
@@ -8,7 +8,7 @@ def sumaPositivos( a, b):
         return -1
     if ( not (type(b) is int) ):
         return -1
-    if ( a >= 0 and b >= 0): 
+    if ( a >= 0 and b >= 0):
         return a + b
 
 def multiplo3o5o15( numero ):
@@ -31,9 +31,23 @@ def palindromo(cadena):
         return False
 
     return cadena == cadena[::-1]
-    
+
+def resta(a,b):
+    if (not (type(a) is int)):
+        return -1
+    if (not (type(b) is int)):
+        return -1
+    return a-b
+
+def multiplicacion(a,b):
+    if (not (type(a) is int)):
+        return -1
+    if (not (type(b) is int)):
+        return -1
+    return a*b
+
 class SoloTest(unittest.TestCase):
-    
+
     def testTrue(self):
         self.assertTrue(devuelveTrue(), "Tiene que fallar")
 
@@ -56,8 +70,17 @@ class SoloTest(unittest.TestCase):
         self.assertEqual(palindromo("aba"), True)
         self.assertEqual(palindromo(1), False)
 
+    def testResta(self):
+        self.assertEqual(resta(50, 20), 30, "Resta correcta")
+        self.assertEqual(resta(50, 80), -30, "Resta correcta")
+        self.assertEqual(resta("a", 10), -1, "Resta incorrecta")
+        self.assertEqual(resta(10, "b"), -1, "Resta incorrecta")
 
-        
+    def testMultiplicacion(self):
+        self.assertEqual(multiplicacion(5, 2), 10, "Resta correcta")
+        self.assertEqual(multiplicacion(-5, 8), -40, "Resta correcta")
+        self.assertEqual(multiplicacion("a", 10), -1, "Resta incorrecta")
+        self.assertEqual(multiplicacion(10, "b"), -1, "Resta incorrecta")
 
 if __name__ == '__main__':
     unittest.main()
